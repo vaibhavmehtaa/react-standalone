f101506f98966c49f9179a5284a7362c2eb018c8
vaibhavmehtaa <vaibhav.mehta@vogo.in>
2019-01-28 03:37:07 +0530

    add webpack and add directories in good shape

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import Reservation from './reservation-form/form'
+import "../css/global.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Reservation />
+    );
+  }
+}
+
+export default hot(module)(App);
